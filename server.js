33a9a42bd2bad30c9406257a4602c5329ca78cd7
shimry92 <152180639+shimry92@users.noreply.github.com>
2025-05-27 02:05:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Tracking System Backend is working!');
+  res.send('Tracking Backend is Running!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
